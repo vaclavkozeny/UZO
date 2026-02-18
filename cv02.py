ba45d69c5c9e01132d4abdf6fdcd4efecbf3229f
Martin Kouble <martin.kouble@tul.cz>
2026-02-18 16:01:04 +0100

    Vypočet vektorizován a byl přidán filtr

--- cv02.py
+++ cv02.py
@@ -12,31 +12,34 @@ template = cv2.imread("cv02/cv02_vzor_hrnecek.bmp")
 hsv_t = cv2.cvtColor(template, cv2.COLOR_RGB2HSV)
 hist_t, b_t = np.histogram(hsv_t[:,:,0], 256, (0, 256))
 hist_t = np.divide(hist_t,max(hist_t))
+hist_t = np.where(hist_t < 0.75, 0, hist_t)
+
 x1 = 100
 y1 = 100
 x2 = 200
 y2 = 200
-new_array = np.zeros((640,480))
+new_array = np.zeros((480,640))
 while True:
     ret, bgr = cap.read()
     if not ret:
         break
     hsv = cv2.cvtColor(bgr, cv2.COLOR_RGB2HSV)
     hist, b = np.histogram(hsv[:,:,0], 256, (0, 256))
-    for x in range(len(new_array)):
-        for y in range(len(new_array[0])):
-            new_array[x][y] = hist_t[hsv[y][x][0]]
-    new_array = np.array(new_array)
-    t_x = 0
-    t_y = 0
-    for x in range(len(new_array)):
-        for y in range(len(new_array[0])):
-            t_x = t_x + x*new_array[x][y]
-            t_y = t_y + y*new_array[x][y]
-    tez_x = int(t_x/np.sum(new_array))
-    tez_y = int(t_y/np.sum(new_array))
+    new_array = hist_t[hsv[...,0]]
 
-    cv2.rectangle(bgr, (tez_x-50, tez_y-100), (tez_x + 50, tez_y + 100), (0, 255, 0))
+    new_array = np.array(new_array)
+    x, y = np.indices(new_array.shape)  # pozor: y = řádky, x = sloupce
+
+    # vážené souřadnice
+    t_x = np.sum(x * new_array)
+    t_y = np.sum(y * new_array)
+
+    total = np.sum(new_array)
+
+    tez_x = int(t_x / total)
+    tez_y = int(t_y / total)
+
+    cv2.rectangle(bgr, (tez_y-50, tez_x-100), (tez_y + 50, tez_x + 100), (0, 255, 0))
     cv2.imshow('Image', bgr)
     key = 0xFF & cv2.waitKey(30)
     if key == 27:
